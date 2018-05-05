fb36e8314e441b4bb4b65540f9d802010fb6435c
Winn Y Cui <pyrinox@Winns-MacBook-Pro.local>
2018-05-05 08:46:47 -0700

    testing out axios module for http requests

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const axios = require('axios')
+
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 // Handles messages events
@@ -33,16 +35,31 @@ function callSendAPI(sender_psid, response) {
 	}
 
 	// Send the HTTP request to the Messenger Platform
-	request({
+	axios({
 		"uri": "https://graph.facebook.com/v2.6/me/messages",
 		"qs": { "access_token": PAGE_ACCESS_TOKEN },
 		"method": "POST",
 		"json": request_body
-	}, (err, res, body) => {
+	})
+	.then((err, res, body)=> {
 		if (!err) {
 			console.log('message sent!')
 		} else {
 			console.error("Unable to send message:" + err);
 		}
-	}); 
+	});
+
+
+	// request({
+	// 	"uri": "https://graph.facebook.com/v2.6/me/messages",
+	// 	"qs": { "access_token": PAGE_ACCESS_TOKEN },
+	// 	"method": "POST",
+	// 	"json": request_body
+	// }, (err, res, body) => {
+	// 	if (!err) {
+	// 		console.log('message sent!')
+	// 	} else {
+	// 		console.error("Unable to send message:" + err);
+	// 	}
+	// }); 
 }
